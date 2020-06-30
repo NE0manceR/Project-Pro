112dd144bffa09261329c7041eda92cf69f3460b
unknown <neomancer2@gmail.com>
2020-06-30 13:57:58 +0300

    'correcteCodoStyle'

--- script.js
+++ script.js
@@ -1,44 +1,34 @@
 let myFullpage = new fullpage('#fullpage', {
-  navigation:'true',
-  navigationPosition:'left',
-  verticalCentered:'false',
-    });
+    navigation: 'true',
+    navigationPosition: 'left',
+    verticalCentered: 'false',
+});
 
 let slidePosition = 0;
 let moveSlide = document.querySelector('.move-slide ');
 
+document.querySelector('.left-btn').addEventListener('click', left = () => {
+    if (slidePosition == -940) {
+        return
+    }
 
-
-document.querySelector('.left-btn').addEventListener('click', left = () =>{
-  if( slidePosition == -940) {
-    return
-  }
-  slidePosition -= 470;
-  console.log(slidePosition);
-  
-
-  
-  moveSlide.style.transform = `translateX(${slidePosition}px)`
-
-})
-document.querySelector('.right-btn').addEventListener('click', right = () =>{
-  if( slidePosition == 0) {
-    return
-  }
-  slidePosition += 470;
-  console.log(slidePosition);
-  
-
-  
-  moveSlide.style.transform = `translateX(${slidePosition}px)`
+    slidePosition -= 470;
+    console.log(slidePosition);
+    moveSlide.style.transform = `translateX(${slidePosition}px)`
 
 })
 
+document.querySelector('.right-btn').addEventListener('click', right = () => {
+    if (slidePosition == 0) {
+        return
+    }
+    slidePosition += 470;
+    console.log(slidePosition);
 
 
+    moveSlide.style.transform = `translateX(${slidePosition}px)`
 
-
-
+})
 
 
 // ========================Partner slider=====
@@ -51,36 +41,32 @@ let check = slides.length / 2;
 let checkMove = 0;
 
 
-
 partnerLeftBtn.addEventListener('click', partnerLeftSlide = (event) => {
-  console.log(document.body.clientWidth);
-  
-  if(checkMove  < check ) {
-    console.log(checkMove);
-    partnerSlidePositiion -= 239;
-    document.querySelector('.partner-slides-wrap').style.transform = `translateX(${partnerSlidePositiion}px)`;
-    checkMove++
-  }
-  
+    console.log(document.body.clientWidth);
+
+    if (checkMove < check) {
+        console.log(checkMove);
+        partnerSlidePositiion -= 239;
+        document.querySelector('.partner-slides-wrap').style.transform = `translateX(${partnerSlidePositiion}px)`;
+        checkMove++
+    }
+
 })
 
 partnerRighttBtn.addEventListener('click', partnerRighttBtn = () => {
-  if (checkMove == 0) {
-    reutrn
-    
-  }
- 
-  if(checkMove <= check  ) {
-    console.log(checkMove);
-    partnerSlidePositiion += 239;
-    document.querySelector('.partner-slides-wrap').style.transform = `translateX(${partnerSlidePositiion}px)`;
-    checkMove--
-  }
-  
+    if (checkMove == 0) {
+        reutrn
 
-  
-  
-  
+    }
+
+    if (checkMove <= check) {
+        console.log(checkMove);
+        partnerSlidePositiion += 239;
+        document.querySelector('.partner-slides-wrap').style.transform = `translateX(${partnerSlidePositiion}px)`;
+        checkMove--
+    }
+
+
 })
 
 
